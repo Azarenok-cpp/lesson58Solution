80708a40488424971b5e0473c9106e6d53cd5f74
work204 <titanistikman@gmail.com>
2025-04-16 17:11:14 +0300

    Add patterns for project

---
+++ task58main_1/logic.cpp
@@ -0,0 +1,5 @@
+#include "logic.h"
+
+int sum_negative_elements(int* array, int size) {
+	return 0;
+}
---
+++ task58main_1/main.cpp
@@ -0,0 +1,9 @@
+#include "logic.h"
+
+int main() {
+	int* array = new int[] {-1, -2, -3, -4, -5, -6, -7, -8, -9};
+
+	cout << "Sum abs of negative elements: ";
+
+	return 0;
+}
